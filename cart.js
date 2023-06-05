226550ff0c568ebfcda724d1bd208986ac17a079
MuradIssin <117025448+MuradIssin@users.noreply.github.com>
2023-06-05 08:33:02 +0600

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,20 +1,34 @@
-'use strict'
-const rainbow = 'padding: 10px 5px; margin-bottom: 6px; border: 1px solid blue; text-shadow: 1px 1px 2px white,  0 0 0.8; color: orange';
+'use strict';
+const rainbow = 'padding: 10px 5px; margin-bottom: 6px; border: 1px solid blue';
+// ; text-shadow: 1px 1px 2px white,  0 0 0.8; color: orange';
 console.log('%c Модуль 4 урок 8 ', rainbow);
 
 console.warn('Задание I ');
 
 const cart = {
-  items: [], //товары
+  items: [], // товары
   totalPrice: 0, // общая стоимость товаров
-  count: 0, //количество товаров
-}
+  count: 0, // количество товаров
+};
 
+const calculateItemPrice = () => {
+  const newTotal = cart.items.reduce((acc, item) => item[1] + acc, 0);
+  return newTotal;
+};
+const increaseCount = (add) => {
+  cart.count += add;
+};
+const add = (nameProduct, priceProduct, quantity = 1) => {
+  const arrayGood = [nameProduct, priceProduct, quantity];
+  cart.items[cart.items.length] = arrayGood;
+  // cart.totalPrice = calculateItemPrice();
+  increaseCount(quantity);
+};
 
 Object.defineProperty(cart, 'totalPrice', {
   get() {
-    return calculateItemPrice()
-  }
+    return calculateItemPrice();
+  },
   // set() {
 
   // }
@@ -25,24 +39,6 @@ Object.defineProperty(cart, 'totalPrice', {
 //   return cart.totalPrice;
 // };
 
-const add = (nameProduct, priceProduct, quantity = 1) => {
-  const arrayGood = [nameProduct, priceProduct, quantity];
-  cart.items[cart.items.length] = arrayGood;
-  // cart.totalPrice = calculateItemPrice();
-  increaseCount(quantity);
-};
-
-const increaseCount = (add) => {
-  cart.count += add
-};
-
-const calculateItemPrice = () => {
-  const newTotal = cart.items.reduce((acc, item) => {
-    return item[1] + acc;
-  }, 0)
-  return newTotal;
-};
-
 const clear = () => {
   cart.items = [];
   cart.totalPrice = 0;
@@ -50,30 +46,29 @@ const clear = () => {
 };
 
 const print = () => {
-  console.log(`текущая корзина :`)
+  console.log(`текущая корзина :`);
   cart.items.forEach((item) => {
-    console.log(`${item[0]}  общая стоимость ${item[1]} количество ${item[2]}`)
+    console.log(`${item[0]}  общая стоимость ${item[1]} количество ${item[2]}`);
   });
-  console.log(`общая стоимость корзины ${cart.totalPrice} уе`)
+  console.log(`общая стоимость корзины ${cart.totalPrice} уе`);
 };
 
-
 // вызываем добавление товара
-add("good", 100, 20);
-add("good2", 1, 200);
-add("good3", 450);
-add("good4", 45, 888);
-add("good5", 450, 88);
-
+add('good', 100, 20);
+add('good2', 1, 200);
+add('good3', 450);
+add('good4', 45, 888);
+add('good5', 450, 88);
 
 print();
 
-console.log("задание 9 урока модуль 4");
+console.log('задание 9 урока модуль 4');
 // cart.totalPrice = 10;
 
 print();
 
-// clear();
+clear();
 // print();
+
 
 
